b39588316c2b7230856b356ba2cd3dc972dee132
ArrangingFear56 <82667894+ArrangingFear56@users.noreply.github.com>
2023-03-17 02:44:55 +0800

    Add files via upload

--- Eurobot_2023_small/src/Eurobot2023_main_small_test/src/better_cake.py
+++ Eurobot_2023_small/src/Eurobot2023_main_small_test/src/better_cake.py
@@ -5,6 +5,7 @@ from std_msgs.msg import Bool
 from std_msgs.msg import Float64
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseArray
 from geometry_msgs.msg import Quaternion
 import math
@@ -13,22 +14,22 @@ from scipy.spatial.distance import euclidean
 from itertools import permutations
 
 # subscribe each cake's position
-browns = [(1125, 725), (1125, 1275), (1875, 725), (1875, 1275)]
-yellows = [(775, 225), (775, 1775), (2225, 225), (2225, 1775)]
-pinks = [(575, 225), (575, 1775), (2425, 225), (2425, 1775)]
+browns = [[1125, 725], [1125, 1275], [1875, 725], [1875, 1275]]
+yellows = [[775, 225], [775, 1775], [2225, 225], [2225, 1775]]
+pinks = [[575, 225], [575, 1775], [2425, 225], [2425, 1775]]
 allCakes = [browns, yellows, pinks]
 
 tempAllCakes = []
 for i in range(3):
-    tempAllCakes.append([[(-1, -1), (-1, -1), (-1, -1), (-1, -1)], [(-1, -1), (-1, -1), (-1, -1), (-1, -1)]])
+    tempAllCakes.append([[[-1, -1], [-1, -1], [-1, -1], [-1, -1]], [[-1, -1], [-1, -1], [-1, -1], [-1, -1]]])
 
 # subscribe each enemy's pos
-enemies = [(1125, 1775), (1875, 225)]
+enemies = [[1125, 1775], [1875, 225]]
 # subscribe our robots pos
-startPos = [(-1, -1), (1875, 1775)]
+startPos = [[-1, -1], [-1, -1]]
 absAng = [0, 0]
 
-picked = [[(-1, -1), (-1, -1), (-1, -1)], [(-1, -1), (-1, -1), (-1, -1)]]
+picked = [[ [-1, -1],  [-1, -1],  [-1, -1]], [ [-1, -1],  [-1, -1],  [-1, -1]]]
 used = []
 got = [[0, 0, 0], [0, 0, 0]]  # brown, yellow, pink
 fullness = [[0, 0, 0, 0], [0, 0, 0, 0]]
@@ -37,11 +38,24 @@ currMin = [99999, 99999]
 minAngle = 360
 outAngle = [[0, 0, 0], [0, 0, 0]]
 
+robotNum = 1
 position = [-1, -1]
 quaternion = Quaternion()
 robotPose = PoseArray()
 
 stop = False
+
+def startPos1_callback(msg):
+    global startPos, absAng
+    startPos[0][0] = msg.pose.pose.position.x * 1000
+    startPos[0][1] = msg.pose.pose.position.y * 1000
+    absAng[0] = quaternion2euler(msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w)
+
+def startPos2_callback(msg):
+    global startPos
+    startPos[1][0] = msg.pose.pose.position.x * 1000
+    startPos[1][1] = msg.pose.pose.position.y * 1000
+    absAng[1] = quaternion2euler(msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w)
 
 def cake_callback(msg):
     if msg.data:
@@ -75,8 +89,9 @@ def robotPublish(num, i, color):
     robotPose.poses.append(pose)
 
     if i == 2:
-        rospy.sleep(1)
+        rospy.sleep(0.3)
         pub.publish(robotPose)
+        # print(robotPose)
 
 def euler2quaternion(roll, pitch, yaw):
     """
@@ -97,6 +112,28 @@ def euler2quaternion(roll, pitch, yaw):
 
     return Quaternion(qx, qy, qz, qw)
 
+def quaternion2euler(x, y, z, w):
+        """
+        Convert a quaternion into euler angles (roll, pitch, yaw)
+        roll is rotation around x in radians (counterclockwise)
+        pitch is rotation around y in radians (counterclockwise)
+        yaw is rotation around z in radians (counterclockwise)
+        """
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+     
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+     
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+     
+        return yaw_z / math.pi * 180 # in radians
+
 def howLong(pos):
     dis = 0
     for i in range(len(pos) - 1):
@@ -108,32 +145,34 @@ def closerEnemy(target):
     speed = 0.9  # enemy/our
     min = 99999
     for enemy in enemies:
-        if enemy != (-1, -1):
+        if enemy != [-1, -1]:
             if min > euclidean(enemy, target) / speed:
                 min = euclidean(enemy, target) / speed
     return min
 
 def whatColorGet(pos, num):
+    global got
     for i in range(3):
         if pos in allCakes[i]:
             got[num][i] = 1
 
 def whatColorReverse(pos, num):
+    global got
     for i in range(3):
         if pos in allCakes[i]:
             got[num][i] = 0
 
 def safest(pos, num):
+    global got, allCakes
     tempColorMin = 99999
-    tempColorPicked = (-1, -1)
+    tempColorPicked =  [-1, -1]
     for i in range(3):
         if got[num][i] != 1:
             for target in allCakes[i]:
-                if target not in used:
+                if target not in used and target != [-1, -1]:
                     if tempColorMin > euclidean(pos, target) - closerEnemy(target):
                         tempColorMin = euclidean(pos, target) - closerEnemy(target)
                         tempColorPicked = target
-    # print(tempColorPicked)
     return tempColorPicked
 
 def where2go(pos, num):
@@ -142,7 +181,7 @@ def where2go(pos, num):
         return []
     tempMin = 99999
     for enemy in enemies:
-        if enemy != (-1, -1):
+        if enemy != [-1, -1]:
             for i in range(3):
                 if got[num][i] != 1:
                     for target in allCakes[i]:
@@ -152,11 +191,11 @@ def where2go(pos, num):
     for i in used:
         for j in range(3):
             if i in tempAllCakes[j][num]:
-                tempAllCakes[j][num][tempAllCakes[j][num].index(i)] = (-1, -1)
+                tempAllCakes[j][num][tempAllCakes[j][num].index(i)] = [-1, -1]
 
     for i in range(3):
         if got[num][i] == 1:
-            tempAllCakes[i][num] = [(99999, 99999), (99999, 99999), (99999, 99999), (99999, 99999)]
+            tempAllCakes[i][num] = [[99999, 99999], [99999, 99999], [99999, 99999], [99999, 99999]]
 
     orders = list(permutations([tempAllCakes[0][num], tempAllCakes[1][num], tempAllCakes[2][num]]))
     # print(tempB[num], tempY[num], tempP[num])
@@ -165,15 +204,15 @@ def where2go(pos, num):
         for i in order[0]:
             for j in order[1]:
                 for k in order[2]:
-                    if order[0] == [(99999, 99999), (99999, 99999), (99999, 99999), (99999, 99999)]:
+                    if order[0] == [[99999, 99999], [99999, 99999], [99999, 99999], [99999, 99999]]:
                         i = pos
-                    if order[1] == [(99999, 99999), (99999, 99999), (99999, 99999), (99999, 99999)]:
+                    if order[1] == [[99999, 99999], [99999, 99999], [99999, 99999], [99999, 99999]]:
                         j = i
-                    if order[2] == [(99999, 99999), (99999, 99999), (99999, 99999), (99999, 99999)]:
+                    if order[2] == [[99999, 99999], [99999, 99999], [99999, 99999], [99999, 99999]]:
                         k = j
                     tempDis = 0
                     enemyDis = 0
-                    if i != (-1, -1) and j != (-1, -1) and k != (-1, -1):
+                    if i != [-1, -1] and j != [-1, -1] and k != [-1, -1]:
                         tempDis = euclidean(pos, i) + euclidean(i, j)
                         enemyDis = closerEnemy(j)
                         if tempDis < enemyDis and tempDis < tempMin:
@@ -184,10 +223,10 @@ def where2go(pos, num):
                                 picked[num] = [i, j, k]
                                 # print(num, picked[num])
                                 for o in order:
-                                    if o == [(99999, 99999), (99999, 99999), (99999, 99999), (99999, 99999)]:
-                                        picked[num][order.index(o)] = (-1, -1)
+                                    if o == [[99999, 99999], [99999, 99999], [99999, 99999], [99999, 99999]]:
+                                        picked[num][order.index(o)] =  [-1, -1]
 
-    if picked[num] == [(-1, -1), (-1, -1), (-1, -1)]:
+    if picked[num] == [[-1, -1], [-1, -1], [-1, -1]]:
         # print("so safe", num)
         picked[num][0] = safest(pos, num)
         whatColorGet(picked[num][0], num)
@@ -205,11 +244,11 @@ def where2go(pos, num):
                 order.remove(pos)
                 picked[num] = order
         for i in range(3):
-            if picked[num][i] != (-1, -1):
+            if picked[num][i] != [-1, -1]:
                 whatColorReverse(picked[num][i], num)
 
     for i in range(3):
-        if picked[num][i] == (-1, -1):
+        if picked[num][i] == [-1, -1]:
             picked[num].remove(picked[num][i])
     if picked[num]:
         anglePos = pos
@@ -239,43 +278,47 @@ def where2go(pos, num):
 
 def listener():
     rospy.init_node("better_cake")
-    rospy.Subscriber("/cake1", Bool, cake_callback)
+    rospy.Subscriber("/cake"+str(robotNum), Bool, cake_callback)
+    rospy.Subscriber("/robot1/odom", Odometry, startPos1_callback)
+    rospy.Subscriber("/robot2/odom", Odometry, startPos2_callback)
     rospy.spin()
 
 def publisher():
-    global quaternion, stop
+    global quaternion, stop, enemies, startPos, currMin, picked, used, robotNum, robotPose
     color = -1
 
-    if enemies[0] == (-1, -1) and enemies[1] == (-1, -1):
-        enemies[0] = (99999, 99999)
+    if enemies[0] ==  [-1, -1] and enemies[1] ==  [-1, -1]:
+        enemies[0] = [99999, 99999]
 
-    for robot in startPos:
-        if robot != (-1, -1):
-            where2go(robot, startPos.index(robot))
+    for robot in range(2):
+        if robot != [-1, -1]:
+            where2go(startPos[robot], robot)
 
     if currMin[0] < currMin[1]:
         used = picked[0]
-        if startPos[1] != (-1, -1):
-            picked[1] = [(-1, -1), (-1, -1), (-1, -1)]
+        if startPos[1] != [-1, -1]:
+            picked[1] = [[-1, -1], [-1, -1], [-1, -1]]
             currMin[1] = 99999
             where2go(startPos[1], 1)
     else:
         used = picked[1]
-        if startPos[0] != (-1, -1):
-            picked[0] = [(-1, -1), (-1, -1), (-1, -1)]
+        if startPos[0] != [-1, -1]:
+            picked[0] = [[-1, -1], [-1, -1], [-1, -1]]
             currMin[0] = 99999
             where2go(startPos[0], 0)
 
-    for robot in range(2):
-        if startPos[robot] != (-1, -1):
-            for pos in range(3):
-                for axis in range(2):                    
-                    for i in range(3):
-                        if picked[robot][pos] in allCakes[i]:
-                            color = i
-                    position[axis] = picked[robot][pos][axis] * 0.001
-                quaternion = euler2quaternion(0, 0, outAngle[robot][pos] * math.pi / 180)
-                robotPublish(robot, pos, color)
+    if startPos[robotNum] != [-1, -1]:
+        robotPose.poses=[]
+        for pos in range(3):
+            for axis in range(2):                    
+                for i in range(3):
+                    if picked[robotNum][pos] in allCakes[i]:
+                        color = i
+                position[axis] = picked[robotNum][pos][axis] * 0.001
+            quaternion = euler2quaternion(0, 0, outAngle[robotNum][pos] * math.pi / 180)
+            robotPublish(robotNum, pos, color)
+            # print(picked)
+
     stop = False
 
 if __name__=="__main__":
--- Eurobot_2023_small/src/Eurobot2023_main_small_test/src/first_cherry.py
+++ Eurobot_2023_small/src/Eurobot2023_main_small_test/src/first_cherry.py
@@ -2,66 +2,111 @@
 
 import rospy
 from std_msgs.msg import Bool
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Pose
 from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseArray
 import numpy as np
 from scipy.spatial.distance import euclidean
 
-cherries = [[(100, 900), (300, 900)], [(1350, 1885), (1650, 1885)], [(2700, 900), (2900, 900)], [(1350, 115), (1650, 115)]]
+cherries = [[[100, 900], [300, 900]], [[1350, 1885], [1650, 1885]], [[2700, 900], [2900, 900]], [[1350, 115], [1650, 115]]]
 
 # subscribe cherries’ existence
 cherry = [1, 1, 1, 1]
 
 # subscribe each enemy's pos
-enemies = [(1125, 1775), (1875, 225)]
+enemies = [[1125, 1775], [1875, 225]]
 # subscribe our robots pos
-startPos = [(-1, -1), (1875, 1775)]
+startPos = [[-1, -1],[-1, -1]]
 
-robotPose = PoseStamped()
-picked = [(-1, -1), (-1, -1)]
+tempMin = [99999, 99999]
+tempSide = [[[-1, -1], [-1, -1]], [[-1, -1], [-1, -1]]]
+used = []
+
+robotNum = 1
+robotPose = PoseArray()
+pickedSide = [[[-1, -1], [-1, -1]], [[-1, -1], [-1, -1]]]
+
+def startPos1_callback(msg):
+    global startPos, absAng
+    startPos[0][0] = msg.pose.pose.position.x * 1000
+    startPos[0][1] = msg.pose.pose.position.y * 1000
+
+def startPos2_callback(msg):
+    global startPos
+    startPos[1][0] = msg.pose.pose.position.x * 1000
+    startPos[1][1] = msg.pose.pose.position.y * 1000
 
 def cherryPublish():
-    global robotPose, picked
-    pub = rospy.Publisher('/cherry_picked1', PoseStamped, queue_size=1000)
-    for i in picked:
-        if i != (-1, -1):
-            robotPose.header.frame_id = "/robot" + str(picked.index(i)+1) + "/map"
-            robotPose.header.stamp = rospy.Time.now()
-            robotPose.pose.position.x = i[0] * 0.001
-            robotPose.pose.position.y = i[1] * 0.001
-            robotPose.pose.orientation.x = 0
-            robotPose.pose.orientation.y = 0
-            robotPose.pose.orientation.z = 0
-            robotPose.pose.orientation.w = 1
-            rospy.sleep(0.3)
-            pub.publish(robotPose)
+    global robotPose, pickedSide
+    pub = rospy.Publisher('/cherry_picked' + str(robotNum), PoseArray, queue_size=1000)
+    side = pickedSide[robotNum]
+    if side != [[-1, -1], [-1, -1]]:
+        robotPose.header.frame_id = "/robot" + str(pickedSide.index(side)+1) + "/map"
+        robotPose.header.stamp = rospy.Time.now()
+
+        for i in side:
+            pose = Pose()
+            pose.position.x = i[0] * 0.001
+            pose.position.y = i[1] * 0.001
+            pose.orientation.x = 0
+            pose.orientation.y = 0
+            pose.orientation.z = 0
+            pose.orientation.w = 1
+            robotPose.poses.append(pose)
+
+        rospy.sleep(0.3)
+        pub.publish(robotPose)
 
 def cherry_callback(msg):
     if msg.data:
         publisher()
 
-def where2suck(pos):
-    tempCherry = (-1, -1)
-    tempMin = 99999
+def where2suck(pos, num):
+    global tempMin, tempSide, used
+    tempMin[num] = 99999
+    tempSide[num] = [[-1, -1], [-1, -1]]
+
     for sides in cherries:
         for cherrySide in sides:
-            dis2cherry = euclidean(pos, cherrySide)
-            if dis2cherry < tempMin:
-                tempMin = dis2cherry
-                tempCherry = cherrySide
-    return tempCherry
+            if cherrySide not in used:
+                dis2cherry = euclidean(pos, cherrySide)
+                if dis2cherry < tempMin[num]:
+                    tempMin[num] = dis2cherry
+                    tempSide[num] = sides
+                    if cherrySide == tempSide[num][1]:
+                        tempSide[num][0], tempSide[num][1] = tempSide[num][1], tempSide[num][0]
+    return tempSide[num]
 
 def listener():
     rospy.init_node("first_cherry")
-    rospy.Subscriber("/cherry1", Bool, cherry_callback)
+    rospy.Subscriber("/cherry" + str(robotNum), Bool, cherry_callback)
     rospy.Subscriber("/cherryExistence", Bool, cherry_callback)
+    rospy.Subscriber("/robot1/odom", Odometry, startPos1_callback)
+    rospy.Subscriber("/robot2/odom", Odometry, startPos2_callback)
     rospy.spin()
 
 def publisher():
-    global robotPose
+    global robotPose, used
     for robot in startPos:
-        if robot != (-1, -1):
-            picked[startPos.index(robot)] = where2suck(robot)
+        if robot != [-1, -1]:
+            pickedSide[startPos.index(robot)] = where2suck(robot, startPos.index(robot))
+
+    if tempMin[0] < tempMin[1]:
+        used = pickedSide[0]
+        if startPos[1] != [-1, -1]:
+            pickedSide[1] = [[-1, -1], [-1, -1]]
+            tempMin[1] = 99999
+            pickedSide[1] = where2suck(startPos[1], 1)
+    else:
+        used = pickedSide[1]
+        if startPos[0] != [-1, -1]:
+            pickedSide[0] = [[-1, -1], [-1, -1]]
+            tempMin[0] = 99999
+            pickedSide[0] = where2suck(startPos[0], 0)
+        
     cherryPublish()
+    print(pickedSide)
 
 if __name__=="__main__":
     try:
