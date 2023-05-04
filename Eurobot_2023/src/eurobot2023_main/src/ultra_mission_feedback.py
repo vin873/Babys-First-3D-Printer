6905b02b49440a69009ee4649f2d80a58be53803
vin873 <109648831+vin873@users.noreply.github.com>
2023-05-04 19:23:27 +0800

    Add files via upload

--- Eurobot_2023/src/eurobot2023_main/src/ultra_mission_feedback.py
+++ Eurobot_2023/src/eurobot2023_main/src/ultra_mission_feedback.py
@@ -7,6 +7,8 @@ from nav_msgs.msg import Odometry
 from std_msgs.msg import Int16MultiArray
 from std_msgs.msg import Int32MultiArray
 
+pre_msg = String()
+
 done = Int16MultiArray()
 done2 = Int32MultiArray()
 df = [0, 0, 0, 0, 0]
@@ -22,50 +24,63 @@ def cherryE_callback(msg):
         cherry[i] = msg.data[i]
 
 def mission_callback(msg):
-    global df, cherry
+    global df, cherry, pre_msg
 
-    if msg.data[0] == 'b' or msg.data[0] == 'y' or msg.data[0] == 'p':
-        df[int(msg.data[1])+1] = 1
-        df[0] = 1
+    if msg.data != pre_msg:
 
-    elif msg.data[0] == 'c':
-        df[0] = 1
-        if run_mode == 'sim':
-            publisher(1)
+        if msg.data[0] == 'b' or msg.data[0] == 'y' or msg.data[0] == 'p':
+            df[int(msg.data[1])+1] = 1
+            df[0] = 1
+            if run_mode == 'sim':
+                publisher3(msg)
+                publisher4(msg)
 
-    elif msg.data[0] == 'o':
-        df[0] = 1
-        df[int(msg.data[1])+1] = 0
-        if run_mode == 'sim':
-            publisher(1)
+        elif msg.data[0] == 'c':
+            df[0] = 1
+            if run_mode == 'sim':
+                publisher3(msg)
+                publisher(1)
+
+        elif msg.data[0] == 'o':
+            df[0] = 1
+            df[int(msg.data[1])+1] = 0
+            if run_mode == 'sim':
+                publisher3(msg)
+                publisher(1)
+            
+        elif msg.data[0] == 's':
+            df[0] = 1
+            if msg.data[1] == '4':
+                cherry[0] = 0
+            elif msg.data[1] == '5':
+                cherry[2] = 0
+            else:
+                cherry[int(msg.data[1])] = 0
+            if run_mode == 'sim':
+                publisher3(msg)
+                publisher(1.5)
+            publisher2()
+
+        elif msg.data[0] == 'v':
+            df[0] = 1
+            if run_mode == 'sim':
+                publisher3(msg)
+                publisher(1)
+
+        elif msg.data[0] == 'u':
+            df[0] = 1
+            publisher3(msg)
+            publisher(5)
+
+            # df = [2, 0, 1, 1 , 1]
+            # publisher(0.5)
+            # publisher3(msg)
+            # df[0] = 1
+
+        elif msg.data[0] == 'f' or msg.data[0] == 'd':
+            df[0] = 1
         
-    elif msg.data[0] == 's':
-        df[0] = 1
-        if msg.data[1] == '4':
-            cherry[0] = 0
-        elif msg.data[1] == '5':
-            cherry[2] = 0
-        else:
-            cherry[int(msg.data[1])] = 0
-        publisher2()
-        if run_mode == 'sim':
-            publisher(1.5)
-
-    elif msg.data[0] == 'v':
-        df[0] = 1
-        if run_mode == 'sim':
-            publisher(1)
-
-    elif msg.data[0] == 'u':
-        df[0] = 1
-        publisher(5)
-
-        # df = [2, 0, 1, 1 , 1]
-        # publisher(0.5)
-        # df[0] = 1
-
-    elif msg.data[0] == 'f' or msg.data[0] == 'd':
-        df[0] = 1
+        pre_msg = msg.data
 
 def fullness_callback(msg):
     global df
@@ -94,8 +109,18 @@ def publisher2():
     global cherry, done2
     pub2 = rospy.Publisher('/cherryExistence', Int32MultiArray, queue_size=1000)
     done2.data = cherry
-    rospy.sleep(0.3)
+    rospy.sleep(1.5)
     pub2.publish(done2)
+
+def publisher3(msg):
+    pub3 = rospy.Publisher('/handshaker'+str(robotNum), String, queue_size=1000)
+    rospy.sleep(0.3)
+    # pub3.publish(msg)
+
+def publisher4(msg):
+    pub4 = rospy.Publisher('/handshakier'+str(robotNum), String, queue_size=1000)
+    rospy.sleep(0.3)
+    # pub4.publish(msg)
 
 if __name__=="__main__":
     try:
