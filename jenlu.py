080fac6e17d003a432531dd44e5d7fe58186fcf4
vin873 <109648831+vin873@users.noreply.github.com>
2023-05-07 10:54:52 +0800

    Add files via upload

--- jenlu.py
+++ jenlu.py
@@ -10,7 +10,7 @@ from geometry_msgs.msg import PoseStamped
 
 robot = ''
 start = False
-finished = -1
+finished = [-1, -1]
 
 missionStr = String()
 
@@ -20,7 +20,7 @@ deliveredSTM = False
 waitSTM = False
 pub = rospy.Publisher('/mission0', String, queue_size = 100)
 
-robotE = [1, 0]
+robotE = [1, 1]
 
 def euler2quaternion(roll, pitch, yaw):
     """
@@ -42,10 +42,10 @@ def euler2quaternion(roll, pitch, yaw):
     return Quaternion(qx, qy, qz, qw)
 
 def pub_till_get():
-    global deliveredAr, waitAr, deliveredSTM, waitSTM
+    global deliveredAr, waitAr, deliveredSTM, waitSTM, pub
     rospy.sleep(0.3)
     pub.publish(missionStr)
-    rospy.loginfo("Mission published!")
+    rospy.loginfo("Mission published!\n")
     # waitAr = True
     # if missionStr.data[0] == 'b' or missionStr.data[0] == 'y' or missionStr.data[0] == 'p' or missionStr.data[0] == 'h':
     #     waitSTM = True
@@ -63,22 +63,22 @@ def pub_till_get():
 def finish1_callback(msg):
     global finished, robot
     if robot == 1:
-        finished = msg.data
-        if finished == 1:
+        finished[0] = msg.data
+        if finished[0] == 1:
             print("Arrived !\n")
             print("======================================== \n")
-        elif finished == 0:
+        elif finished[0] == 0:
             print("Failed !\n")
             print("======================================== \n")
 
 def finish2_callback(msg):
     global finished, robot
     if robot == 2:
-        finished = int(msg.data)
-        if finished == 1:
+        finished[1] = int(msg.data)
+        if finished[1] == 1:
             print("Arrived !\n")
             print("======================================== \n")
-        elif finished == 0:
+        elif finished[1] == 0:
             print("Failed !\n")
             print("======================================== \n")
 
@@ -128,15 +128,15 @@ def point(m, robotNum, x, y, angle):
 
         mode = m
         if mode == 'p' or mode == 'P':
-            robotPose.header.frame_id = 'path'
+            robotPose.header.frame_id = 'path_10'
         elif mode == 'd' or mode == 'D':
-            robotPose.header.frame_id = 'dock_mov_cake'
+            robotPose.header.frame_id = 'dock_5_mov_cake'
         elif mode == 'c' or mode == 'C':
-            robotPose.header.frame_id = 'dock_mov_cherry'
+            robotPose.header.frame_id = 'dock_5_mov_cherry'
         elif mode == 'r' or mode == 'R':
-            robotPose.header.frame_id = 'dock_rot'
+            robotPose.header.frame_id = 'dock_5_rot_cherry'
         elif mode == 'v' or mode == 'V':
-            robotPose.header.frame_id = 'dock_vibrate'
+            robotPose.header.frame_id = 'dock_5_vibrate'
 
         robotPose.header.stamp = rospy.Time.now()
         robot = robotNum
@@ -157,33 +157,33 @@ def point(m, robotNum, x, y, angle):
                     robotPose.pose.orientation.y = ang.y
                     robotPose.pose.orientation.z = ang.z
                     robotPose.pose.orientation.w = ang.w
-            rospy.sleep(0.3)
+            rospy.sleep(0.5)
             pub.publish(robotPose)
             if mode != 'v' and mode != 'V':
                 if mode != 'r' and mode != 'R':
                     if mode != 'D' and mode != 'd':
-                        print("Heading over to x : [", robotPose.pose.position.x, "] y : [", robotPose.pose.position.x, "] ang : [", angle, "]")
+                        print("Heading over to x : [", robotPose.pose.position.x, "] y : [", robotPose.pose.position.y, "] ang : [", angle, "]")
                     else:
-                        print("Heading over to x :", robotPose.pose.position.x, "y :", robotPose.pose.position.x)
+                        print("Heading over to x : [", robotPose.pose.position.x, "] y : [", robotPose.pose.position.y, "]")
                 else:
                     print("Heading over to ang :", angle)
             else:
                 print("Target Published !!\n")
-            finished = -1
+            finished[robotNum-1] = -1
 
             while not rospy.is_shutdown():
-                if finished == 1 or finished == 0:
+                if finished[robotNum-1] == 1 or finished[robotNum-1] == 0:
                     break
 
-def mission(robotNum, m):
-    global finished, robot, missionStr
+def mission(robotNum, m, check):
+    global finished, robot, missionStr, pub
 
     if not rospy.is_shutdown():
         missionStr = String()
         
         robot = robotNum
         
-        if robotE[robot-1] == 1:
+        if robotE[robot-1] == 1 and (not check or (check and finished[robotNum-1])):
             if robot == 1 or robot == 2:
                 pub = rospy.Publisher('/mission'+str(int(robot)-1), String, queue_size = 100)
 
@@ -225,57 +225,63 @@ def test():
     while not rospy.is_shutdown():
         if start:
             break
-    # t = rospy.get_rostime().to_sec()
-    # mission(1, 'y1')
-    # mission(2, 'y3')
-    # point('p', 1, 1.5, 1, 45)
-    # point('p', 1, 1.125, 0.225, 45)
-    # point('d', 1, 0.87, 0.225, 45)
-    # point('d', 2, 2.13, 0.225, 45)
-    # mission(1, 'c1')
-    # mission(2, 'c3')
-    # rospy.sleep(1)
-    # mission(1, 'p0')
-    # mission(2, 'p2')
-    # point('p', 1, 0.87, 0.225, 135)
-    # point('p', 2, 2.13, 0.225, 135)
-    # point('d', 1, 0.67, 0.225, 135)
-    # point('d', 2, 2.33, 0.225, 135)
-    # mission(1, 'c0')
-    # mission(2, 'c2')
-    # rospy.sleep(1)
-    # point('p', 1, 0.225, 0.225, 135)
-    # point('p', 2, 2.775, 0.725, 135)
-    # mission(1, 'o0')
-    # mission(1, 'o1')
-    # mission(2, 'o2')
-    # mission(2, 'o3')
-    # point('d', 1, 0.45, 0.45, 135)
-    # point('d', 2, 2.55, 0.5, 135)
-    # mission(1, 'c0')
-    # mission(1, 'c1')
-    # mission(2, 'o2')
-    # mission(2, 'o3')
-    # rospy.sleep(1)
-    # point('p', 1, 1.65, 0.225, 135)
-    # point('p', 2, 2.1, 0.225, 135)
-    # mission(1, 'f0')
-    # mission(2, 'f0')
-    # print("Total time : %.1f"%(rospy.get_rostime().to_sec()-t))
+    t = rospy.get_rostime().to_sec()
+    mission(1, 'y1', 0)
+    mission(2, 'y3', 0)
+    point('p', 1, 1.5, 1, 45)
+    point('p', 1, 1.125, 0.225, 45)
+    point('d', 1, 0.87, 0.225, 45)
+    point('p', 2, 1.875, 0.225, 45)
+    point('d', 2, 2.13, 0.225, 45)
+    mission(1, 'c1', 0)
+    mission(2, 'c3', 0)
+    rospy.sleep(1)
+    mission(1, 'p0', 0)
+    mission(2, 'p2', 0)
+    point('p', 1, 0.87, 0.225, 135)
+    point('p', 2, 2.13, 0.225, 135)
+    point('d', 1, 0.67, 0.225, 135)
+    point('d', 2, 2.33, 0.225, 135)
+    mission(1, 'c0', 0)
+    mission(2, 'c2', 0)
+    rospy.sleep(1)
+    point('p', 1, 0.225, 0.225, 135)
+    point('p', 2, 2.775, 0.725, 135)
+    mission(1, 'o0', 1)
+    mission(1, 'o1', 1)
+    mission(2, 'o2', 1)
+    mission(2, 'o3', 1)
+    point('d', 1, 0.45, 0.45, 135)
+    point('d', 2, 2.55, 0.5, 135)
+    mission(1, 'c0', 0)
+    mission(1, 'c1', 0)
+    mission(2, 'c2', 1)
+    mission(2, 'c3', 1)
+    rospy.sleep(1)
+    point('p', 1, 1.65, 0.225, 135)
+    point('p', 2, 2.1, 0.225, 135)
+    mission(1, 'f0', 0)
+    mission(2, 'f0', 0)
+    print("Total time : %.1f"%(rospy.get_rostime().to_sec()-t))
 
-    point('p', 1, 1.6, 0.185, 90)
-    mission(1, 's3')
-    point('c', 1, 1.25, 0.185, 90)
-    mission(1, 'v0')
-    rospy.sleep(2)
-    point('p', 1, 0.4, 0.84, 270)
-    mission(1, 's0')
-    point('c', 1, 0.13, 0.84, 270)
-    mission(1, 'v0')
-    rospy.sleep(2)
-    point('p', 1, 0.225, 0.225, 180)
-    point('d', 1, 0.15, 0.225, 180)
-    point('v', 1, 0.15, 0.225, 180)
+    # point('p', 2, 1.15, 0.181, 90)
+    # point('r', 2, 1.15, 0.181, 90)
+    # mission(2, 's3', 1)
+    # point('c', 2, 1.6, 0.181, 90)
+    # mission(2, 'v0', 0)
+    # rospy.sleep(2)
+    # point('p', 2, 0.225, 0.225, 180)
+    # point('d', 2, 0.16, 0.225, 180)
+    # mission(2, 'u0', 0)
+    # rospy.sleep(2)
+    # point('p', 1, 0.4, 0.84, 270)
+    # mission(1, 's0', 1)
+    # point('c', 1, 0.13, 0.84, 270)
+    # mission(1, 'v0', 0)
+    # rospy.sleep(2)
+    # point('p', 1, 0.225, 0.225, 180)
+    # point('d', 1, 0.15, 0.225, 180)
+    # point('v', 1, 0.15, 0.225, 180)
 
 if __name__=="__main__":
     try:
